47073eaf31f38730d917e0be063ce86838981127
zhoutong <zhoutongsite@gmail.com>
2020-11-23 18:55:53 +0800

    创建web服务器

--- src/main.js
+++ src/main.js
@@ -1,2 +1,8 @@
-const greeting='您好 ~';
-console.log(greeting);
+const http=require('http')
+const server=http.createServer((request,response)=>{
+    response.write('wow')
+    response.end()
+})
+server.listen(3000,()=>{
+    console.log('http://localhost:3000');
+})
