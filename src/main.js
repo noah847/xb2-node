3299b3b16209cc1dec4a80104372cb4fcca7c2c8
zhoutong <zhoutongsite@gmail.com>
2020-11-26 20:21:46 +0800

    请求和响应

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 const http=require('http')
 const server=http.createServer((request,response)=>{
-    response.write('wow')
+    console.log(request.headers["user-agent"]);
+    response.writeHead(200, {
+        'Content-Type': 'text/html',
+      });
+    response.write('<input>')
     response.end()
 })
 server.listen(3000,()=>{
