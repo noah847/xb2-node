b21a63497351eb31048c598adcb3c598a9cb7c75
zhoutong <zhoutongsite@gmail.com>
2020-11-27 18:58:04 +0800

    请求的地址

--- src/main.js
+++ src/main.js
@@ -1,12 +1,22 @@
-const http=require('http')
-const server=http.createServer((request,response)=>{
-    console.log(request.headers["user-agent"]);
-    response.writeHead(200, {
-        'Content-Type': 'text/html',
-      });
-    response.write('<input>')
-    response.end()
+const http = require('http')
+const server = http.createServer((request, response) => {
+    switch (request.url) {
+        case '/':
+            response.write('hola')
+            break;
+        case '/posts':
+            response.write('posts')
+            break;
+        case '/signup':
+            response.write('signup')
+            break;
+        default:
+            response.writeHead(404);
+            response.write('404');
+            break;
+    }
+    response.end();
 })
-server.listen(3000,()=>{
+server.listen(3000, () => {
     console.log('http://localhost:3000');
 })
