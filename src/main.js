af23ef1eeba2eef39991af788c4eb7cb54b6fda5
zhoutong <zhoutongsite@gmail.com>
2020-12-12 15:47:52 +0800

    定义响应 JSON 数据的应用接口

--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
-const { request } = require("express");
-
 const express = require('express')
 const app = express()
 const port = 3000
@@ -12,8 +10,25 @@ app.get('/', (req, res) => {
     res.send('你好')
 })
 
-const data = {
-    id: 1,
-    title: '关山月',
-    content: '明月出天山，苍茫云海间'
-}
+const data = [
+    {
+        id: 1,
+        title: '关山月',
+        content: '明月出天山，苍茫云海间'
+    },
+    {
+        id: 2,
+        title: '望岳',
+        content: '会当凌绝顶，一览众山小'
+    },
+    {
+        id: 1,
+        title: '忆江南',
+        content: '日出江花红胜火，春来江水绿如蓝'
+    },
+]
+
+app.get('/posts', (req, res) => {
+    res.send(data)
+})
+
