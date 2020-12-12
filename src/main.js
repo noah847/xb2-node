c8460169231bba06aad83a576f45f3b8c72582eb
zhoutong <zhoutongsite@gmail.com>
2020-12-12 15:38:39 +0800

    准备演示数据

--- src/main.js
+++ src/main.js
@@ -12,20 +12,8 @@ app.get('/', (req, res) => {
     res.send('你好')
 })
 
-// const http = require('http')
-// const server = http.createServer((request, response) => {
-//     const data = {
-//         id: 1,
-//         title: '关山月',
-//         content: '明月出天山，苍茫云海间'
-//     }
-//     const jsonData = JSON.stringify(data);
-//     response.writeHead(200, {
-//         'Content-Type': 'application/json; charset=utf-8',
-//     })
-//     response.write(jsonData)
-//     response.end();
-// })
-// server.listen(3000, () => {
-//     console.log('http://localhost:3000');
-// })
+const data = {
+    id: 1,
+    title: '关山月',
+    content: '明月出天山，苍茫云海间'
+}
