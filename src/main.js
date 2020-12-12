10077762f6c6148ce13c82e50dc99dc89c91e66c
zhoutong <zhoutongsite@gmail.com>
2020-12-12 15:16:46 +0800

    创建 Web 服务器<Express.js>

--- src/main.js
+++ src/main.js
@@ -1,17 +1,27 @@
-const http = require('http')
-const server = http.createServer((request, response) => {
-    const data = {
-        id: 1,
-        title: '关山月',
-        content: '明月出天山，苍茫云海间'
-    }
-    const jsonData = JSON.stringify(data);
-    response.writeHead(200, {
-        'Content-Type': 'application/json; charset=utf-8',
-    })
-    response.write(jsonData)
-    response.end();
+const { request } = require("express");
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.listen(port,()=>{
+    console.log('🚀服务已启动');
 })
-server.listen(3000, () => {
-    console.log('http://localhost:3000');
-})
+
+// const http = require('http')
+// const server = http.createServer((request, response) => {
+//     const data = {
+//         id: 1,
+//         title: '关山月',
+//         content: '明月出天山，苍茫云海间'
+//     }
+//     const jsonData = JSON.stringify(data);
+//     response.writeHead(200, {
+//         'Content-Type': 'application/json; charset=utf-8',
+//     })
+//     response.write(jsonData)
+//     response.end();
+// })
+// server.listen(3000, () => {
+//     console.log('http://localhost:3000');
+// })
